de42cda8b91a74ec28e1cf3f7a34a5503be7705b
myosubug <seanoh.dev@gmail.com>
2018-09-22 01:10:04 -0600

    program completed with similar word suggestion and capital letter handling in title

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def dictionary(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif len(get_close_matches(w, data.keys())):
+        yn = input("did you mean '%s'? Enter Y/y if yes, N/n if no.\n" % get_close_matches(w, data.keys())[0])
+        if yn == "Y" or yn =="y":
+            return data[get_close_matches(w, data.keys())[0]]
+        else:
+            return "There is no such word :(, please check your word again!"
     else:
         return "The word doens't exist in the dictionary, please check your word again"
 
 word = input("Enter word: ")
-print(dictionary(word))
+output = dictionary(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
